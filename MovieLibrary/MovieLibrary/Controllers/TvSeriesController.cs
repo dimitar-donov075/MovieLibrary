055f1f8533d60056acbcfee3dc12dec4def7c16a
dimitar-donov075 <dimitar.donov96@gmail.com>
2016-06-07 01:16:52 +0300

    AddedMoviesTvSeriesAnimes

---
+++ MovieLibrary/MovieLibrary/Controllers/TvSeriesController.cs
@@ -0,0 +1,134 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using MovieLibrary.Models;
+using Microsoft.AspNet.Identity.Owin;
+using Microsoft.AspNet.Identity;
+
+namespace MovieLibrary.Controllers
+{
+    [Authorize]
+    public class TvSeriesController : Controller
+    {
+        private ApplicationDbContext db = new ApplicationDbContext();
+
+        // GET: /TvSeries/
+        public ActionResult Index()
+        {
+            var id = User.Identity.GetUserId();
+            var seriesByUser = db.TvSeries.Where(c => c.ApplicationUserId.Id == id);
+            return View(seriesByUser.ToList());
+        }
+
+        // GET: /TvSeries/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            TvSeries tvseries = db.TvSeries.Find(id);
+            if (tvseries == null)
+            {
+                return HttpNotFound();
+            }
+            return View(tvseries);
+        }
+
+        // GET: /TvSeries/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: /TvSeries/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include="Id,Title,Genre,AirDate,Season,Episode")] TvSeries tvseries)
+        {
+            if (ModelState.IsValid)
+            {
+                var id = User.Identity.GetUserId();
+                tvseries.ApplicationUserId = db.Users.Find(id);
+                db.TvSeries.Add(tvseries);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(tvseries);
+        }
+
+        // GET: /TvSeries/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            TvSeries tvseries = db.TvSeries.Find(id);
+            if (tvseries == null)
+            {
+                return HttpNotFound();
+            }
+            return View(tvseries);
+        }
+
+        // POST: /TvSeries/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include="Id,Title,Genre,AirDate,Season,Episode")] TvSeries tvseries)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(tvseries).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(tvseries);
+        }
+
+        // GET: /TvSeries/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            TvSeries tvseries = db.TvSeries.Find(id);
+            if (tvseries == null)
+            {
+                return HttpNotFound();
+            }
+            return View(tvseries);
+        }
+
+        // POST: /TvSeries/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            TvSeries tvseries = db.TvSeries.Find(id);
+            db.TvSeries.Remove(tvseries);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ MovieLibrary/MovieLibrary/Models/IdentityModels.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNet.Identity.EntityFramework;
+using System.Collections.Generic;
+using System.Data.Entity;
+
+namespace MovieLibrary.Models
+{
+    // You can add profile data for the user by adding more properties to your ApplicationUser class, please visit http://go.microsoft.com/fwlink/?LinkID=317594 to learn more.
+    public class ApplicationUser : IdentityUser
+    {
+        public virtual ICollection<Movie> Movies { get; set; }
+        public virtual ICollection<Anime> Animes { get; set; }
+        public virtual ICollection<TvSeries> TvSeries { get; set; }
+    }
+
+    public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
+    {
+        public ApplicationDbContext()
+            : base("DefaultConnection")
+        {
+        }
+        public DbSet<Anime> Animes { get; set; }
+        public DbSet<Movie> Movies { get; set; }
+        public DbSet<TvSeries> TvSeries { get; set; }
+
+    }
+}
